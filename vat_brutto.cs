644b6cf4d5e1fd4d5c6160fdffd9d8f9e9f0911b
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:06:15 +0200

    Create vat_brutto.cs

---
+++ vat_brutto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zadanie_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program oblicza stawke VAT i cene brutto na podstawie ceny netto");
+            Console.WriteLine("Podaj cene netto: ");
+
+            double cenaNetto = double.Parse(Console.ReadLine());
+            double VAT = 22;
+            double stawkaVAT = cenaNetto * (VAT / 100);
+            double cenaBrutto = cenaNetto + stawkaVAT;
+
+            Console.WriteLine("Cena brutto wynosi: " + String.Format("{0:N2}", cenaBrutto));
+            Console.WriteLine("Obliczona stawka VAT wynosi: " + String.Format("{0:N2}", stawkaVAT));
+            Console.ReadKey();
+        }
+    }
+}
