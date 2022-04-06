3ff82a2b0c1cba35661cf0fbbbb522bdced20f4f
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:05:35 +0200

    Create parzysta_nieparzysta.cs

---
+++ parzysta_nieparzysta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zadanie_6
+{
+    class Program
+    {
+        private const int V = 2;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe");
+            float a = float.Parse(Console.ReadLine());
+            if ((float)a % V == 0)
+            {
+                Console.WriteLine("parzysta");
+            }
+            else
+            {
+                Console.WriteLine("nieparzysta");
+            }
+        }
+    }
+}
