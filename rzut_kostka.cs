ca23ec8758082b3938429820e9e020c356679b28
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:06:07 +0200

    Create rzut_kostka.cs

---
+++ rzut_kostka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Symulacja serii rzutów kostką sześcienną");
+            Console.WriteLine("Podaj liczbę rzutów: ");
+            var x = GetInput();
+
+            for (int i = x; i > 0; i--)
+            {
+                Random rnd = new Random();
+                int liczba = rnd.Next() % 6 + 1;
+                Console.WriteLine("     " + liczba);
+            }
+        }
+        private static int GetInput()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
