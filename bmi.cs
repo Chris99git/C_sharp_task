6a943450e29c23bc14c05d49675b7080dd8a09d5
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:04:52 +0200

    Create bmi.cs

---
+++ bmi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zadanie_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void z7()
+            {
+                Console.WriteLine("Waga:");
+                decimal waga = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Wzrost:");
+                decimal wzrost = Convert.ToDecimal(Console.ReadLine());
+
+                
+                Console.WriteLine("{0},{1} = {2}", wzrost, waga, wzrost / waga);
+                Console.ReadKey();
+            }
+        }
+    }
+}
