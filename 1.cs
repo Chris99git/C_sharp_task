f1d684d572f392b907358bccf6a0a1aa9586aae1
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:04:28 +0200

    Create 1.cs

---
+++ 1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dane = new int[10];
+            for (int i = 0; i <= dane.Length - 1; i++)
+            {
+                dane[i] = i;
+                Console.WriteLine("dane[" + i + "] = " + i);
+            }
+        }
+    }
+}
