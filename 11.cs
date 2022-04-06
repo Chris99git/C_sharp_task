9781b6e4ec8d80b860dc5c637892aafad3e97223
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:07:28 +0200

    Create 11.cs

---
+++ 11.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace dodatek_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random zmienna = new Random(); // Utworzenie zmiennej, ktora bedzie losować. 
+            int[] tab = new int[200]; //Deklaracja tablicy
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = zmienna.Next(1, 200); //Losowanie i przypisanie wylosowanej liczby do miejsca w tablicy
+            }
+            double suma = 0;
+            foreach (int i in tab)
+            {
+                suma = suma + i; //obliczanie sumy wartosci z tablicy.
+            }
+            Console.WriteLine("Suma to\t" + suma);
+            Console.ReadKey();
+        }
+    }
+}
