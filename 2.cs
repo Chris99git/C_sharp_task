079c70d838d7e027c5e5077ee9fd8e28a64e034f
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:06:55 +0200

    Create 2.cs

---
+++ 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace zadanie_2
+{
+    class cw2
+    {
+        static void Main(string[] args)
+        {
+            int k = 9;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tab[" + i + "] = " + k);
+                k--;
+            }
+        }
+    }
+}
+
+/*
+//zad2
+class cw2
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Program wyswietla zawartosc tablicy jednowymiarowej 10-elementowej");
+        int[] tab = new int[10];
+        for (int i = 9; i >= 0; i--)
+        {
+            tab[i] = i;
+            Console.WriteLine("Tab[" + tab[i] + "] = " + tab[i]);
+        }
+    }
+
+}
+}
+*/
