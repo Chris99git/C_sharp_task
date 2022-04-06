6d38e1e6b51943b970af165539ed9d2875fa6477
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:06:00 +0200

    Create Osoba.cs

---
+++ project1/Osoba.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania
+{
+    class Osoba
+    {
+        private String imie;
+
+        public Osoba(String imie)
+        {
+            this.imie = imie;
+        }
+        public override string ToString()
+        {
+            return this.imie;
+        }
+    }
+}
