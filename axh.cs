44e785eeba5a355043c19507a20cda8908e5a9b6
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:04:43 +0200

    Create axh.cs

---
+++ axh.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class zad1
+{
+    private double a, h, wynik;
+    public void czytaj_dane()
+    {
+        Console.WriteLine("Podaj a i pozniej wysokosc : ");
+        this.a = double.Parse(Console.ReadLine());
+        this.h = double.Parse(Console.ReadLine());
+    }
+    public void przetworz_dane()
+    {
+        this.wynik = (this.a * this.h) / 2;
+    }
+    public void wyswietl_wynik()
+    {
+        Console.WriteLine("Wynik to: " + this.wynik);
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        zad1 z = new zad1();
+        z.czytaj_dane();
+        z.przetworz_dane();
+        z.wyswietl_wynik();
+    }
+}
