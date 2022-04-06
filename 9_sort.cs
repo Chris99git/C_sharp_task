6da5dbb627ff78e856c31f4ed4f517b108e7356d
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:04:39 +0200

    Create 9_sort.cs

---
+++ 9_sort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace zadanie_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sort, posortowane elementy tablicy");
+            int[] tab = { 4, 2, 6, 23, 1, 3, 7, 0 };
+            Array.Sort(tab);
+
+            //for (int i = 0; i < tab.Length; i++) { Console.Write(tab[i] + " "); }
+
+            foreach (int i in tab)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\npress any key to exit the process...");
+            Console.ReadKey();
+        }
+    }
+}
