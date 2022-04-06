cba158c8d7add17133aaf2b93d0c33de6a56a2c7
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:06:47 +0200

    Create 3pitagorejska.cs

---
+++ 3pitagorejska.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Sprawdzanie trójki pitagorejskiej");
+
+            Console.WriteLine("Podaj 1 liczbę:");
+            var a = GetInput();
+
+            Console.WriteLine("Podaj proszę 2 liczbę:");
+            var b = GetInput();
+
+            Console.WriteLine("Podaj proszę 3 liczbę:");
+            var c = GetInput();
+
+            if (a + b > c && a + c > b && b + c > a)
+                Console.WriteLine("Liczby stanowią trójke pitagorejska");
+            else
+                Console.WriteLine("To nie jest trójka pitagorejska");
+            return 0;
+
+        }
+        private static int GetInput()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
