7161295b90973ca467cc65d76582cad98d6a6809
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:05:40 +0200

    Create pomieszaneSlowo.cs

---
+++ pomieszaneSlowo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _3
+{
+    class Program
+    {
+        
+
+        static int Main(string[] args)
+        {
+            string sekretneSlowo, pomieszaneSlowo, slowoUzytkownika;
+
+            Console.WriteLine("Wpisz słowo");
+            sekretneSlowo = Console.ReadLine();
+            pomieszaneSlowo = mieszajLitery(sekretneSlowo);
+
+            do
+            {
+                Console.WriteLine("Co to za słowo?" + pomieszaneSlowo);
+                slowoUzytkownika = Console.ReadLine();
+
+                if (slowoUzytkownika == sekretneSlowo)
+                {
+                    Console.WriteLine("Brawo!");
+                }
+                else
+                {
+                    Console.WriteLine("ŹLE!");
+                }
+            }
+            while (slowoUzytkownika != sekretneSlowo);
+            return 0;
+
+        }
+
+
+        private static string mieszajLitery(string slowo)
+        {
+
+            Random rnd = new Random();
+            string mix = " ";
+
+            while (slowo.Length > 0)
+            {
+                
+                int pozycja = rnd.Next(slowo.Length);  
+                mix += slowo[pozycja];
+                slowo = slowo.Remove(pozycja, 1);
+            }
+
+            return mix;
+        }
+
+
+
+    }
+}
