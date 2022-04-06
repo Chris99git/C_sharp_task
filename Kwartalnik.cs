efe60a2c7a6b17acf1dde875006e041ff7839373
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:05:18 +0200

    Create Kwartalnik.cs

---
+++ Kwartalnik.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Gazeta;
+
+namespace Kwartalnik
+{
+    class Kwartalnik : Gazeta.Gazeta
+    {
+        public Kwartalnik(string isbn, string miesiac, string redaktor)
+        {
+            this.isbn = isbn;
+            this.miesiac = miesiac;
+            this.redaktor = redaktor;
+        }
+
+        public string isbn { get; set; }
+        public string miesiac { get; set; }
+        public string redaktor { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public abstract void Typ();
+
+        public virtual void WyswietlDane()
+        {
+           
+            Console.Write(this.nazwa);
+            Console.Write(this.wydawnictwo);
+            Console.Write(this.rokZalozenia);
+            Console.Write(this.rokOstNum);
+            Console.Write(this.iloscWydan);
+            Console.Write(this.liczbaStron);
+            Console.Write(this.cena);
+            
+        }
+
+
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
