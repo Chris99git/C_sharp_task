392edc96e1ecf1f3e80fc0f232d1c3ed2fb0d5c9
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:05:56 +0200

    Create Kurs1.cs

---
+++ project1/Kurs1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania
+{
+    class Kurs1
+    {
+        private String nazwa;
+        private double liczbaGodzin;
+        private Osoba osoba;
+        private Grupa[] grupy;
+
+        public Kurs1(String nazwa, double liczbaGodzin, Osoba osoba, Grupa[] grupy)
+        {
+            this.nazwa = nazwa;
+            this.liczbaGodzin = liczbaGodzin;
+            this.osoba = osoba;
+            this.grupy = grupy;
+        }
+        public void wyswietl()
+        {
+            Console.WriteLine("Nazwa: " + this.nazwa);
+            Console.WriteLine("Liczba godzin: " + this.liczbaGodzin);
+            Console.WriteLine("Prowadzący: " + this.osoba);
+            Console.WriteLine("Grupy ćwiczeniowe: ");
+            grupy.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+        }
+    }
+}
