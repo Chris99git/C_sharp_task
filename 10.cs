d2e2472b7921465e3f93e7eaa688b1e9da3792b3
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:07:32 +0200

    Create 10.cs

---
+++ 10.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace zadanie_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tab = { "Ala", "Agata", "Ela", "Gienia", "Ola", "Ela", "Tola", "Ela" };
+            Console.WriteLine("Pierwsze wystąpienie ala " + Array.IndexOf(tab, "Ala"));
+            Console.WriteLine("Pierwsze wystąpienie ewa " + Array.IndexOf(tab, "Ela"));
+            Console.WriteLine("Pierwsze wystąpienie władysław " + Array.IndexOf(tab, "Władysław"));
+
+        }
+    }
+}
