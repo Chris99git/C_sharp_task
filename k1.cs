a6a10c6b2fa8cd042d72b56b301284cd2939a32d
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:05:05 +0200

    Create k1.cs

---
+++ k1.cs
@@ -0,0 +1,32 @@
+﻿using System; 
+namespace zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj ilość danych min 5");
+            int x;
+            x = int.Parse(Console.ReadLine());
+            int[]dane = new int[x];
+            if (x>4)
+            {
+                for (int i = 0; i < dane.Length; i++)
+                {
+                    Console.WriteLine($"Podaj {i + 1} dane: ");
+                    dane[i] = int.Parse(Console.ReadLine());
+                }
+
+                foreach (var item in dane)
+                {
+                    Console.Write($"{"y= " + item * 3},");
+                }
+            }
+            else
+            {
+                Console.WriteLine("podaj większą liczbę danych");
+            }
+        }
+    }
+}
+
