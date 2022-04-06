02e1c04cc2b8b9e4ac9c598718ebf610a82ad787
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:05:26 +0200

    Create max.cs

---
+++ max.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zadanie_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe 1");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj liczbe 2");
+            int b = int.Parse(Console.ReadLine());
+
+            int max = a > b ? a : b;
+
+            Console.WriteLine("max: "+ max);
+        
+
+        }
+    }
+}
