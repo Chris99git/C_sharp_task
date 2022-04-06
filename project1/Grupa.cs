c5132f5e44e0ecf2e545f0c25119dd1de81562f2
Chris99git <krzysztofligas7@gmail.com>
2022-04-06 19:05:51 +0200

    Create Grupa.cs

---
+++ project1/Grupa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania
+{
+    class Grupa
+    {
+        private String nazwa;
+
+        public Grupa(String nazwa)
+        {
+            this.nazwa = nazwa;
+        }
+
+        public override string ToString()
+        {
+            return this.nazwa.ToString();
+        }
+    }
+}
